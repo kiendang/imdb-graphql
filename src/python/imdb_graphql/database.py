ea33f4c5a15b6e4978d39334da3881ef84d1a9bf
Kien Dang <mail@kien.ai>
2019-12-11 18:32:53 +0800

    Change user name

--- src/python/imdb_graphql/database.py
+++ src/python/imdb_graphql/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql://dbuser:@/imdb')
+engine = create_engine('postgresql://imdb:@/imdb')
 
 session = scoped_session(sessionmaker(
     autocommit=False,
