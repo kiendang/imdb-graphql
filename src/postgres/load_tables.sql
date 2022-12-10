a05637de8afe73c9d8fd59e157e6bd9b4bf65020
Kien Dang <mail@kien.ai>
2022-12-10 21:37:16 +0800

    Load data from gz

--- src/postgres/load_tables.sql
+++ src/postgres/load_tables.sql
@@ -1,7 +1,9 @@
-\copy titles(tconst, titletype, primarytitle, originaltitle, isadult, startyear, endyear, runtimeminutes, genres) from 'titles.tsv' delimiter E'\t' null as 'NULL' csv header quote E'\b';
-\copy akas from 'akas.tsv' delimiter E'\t' null as 'NULL' csv header quote E'\b';
-\copy episodes from 'episodes.tsv' delimiter E'\t' null as 'NULL' csv header quote E'\b';
-\copy ratings from 'ratings.tsv' delimiter E'\t' null as 'NULL' csv header quote E'\b';
-\copy people(nconst, primaryname, birthyear, deathyear, primaryprofession, knownfortitles) from 'people.tsv' delimiter E'\t' null as 'NULL' csv header quote E'\b';
-\copy principals from 'principals.tsv' delimiter E'\t' null as 'NULL' csv header quote E'\b';
-\copy crew from 'crew.tsv' delimiter E'\t' null as 'NULL' csv header quote E'\b';
+\cd :data_dir
+
+\copy titles(tconst, titletype, primarytitle, originaltitle, isadult, startyear, endyear, runtimeminutes, genres) from program 'gzip -dc titles.basics.tsv.gz' delimiter E'\t' null as 'NULL' csv header quote E'\b';
+\copy akas from program 'gzip -dc title.akas.tsv.gz' delimiter E'\t' null as 'NULL' csv header quote E'\b';
+\copy episodes from program 'gzip -dc title.episode.tsv.gz' delimiter E'\t' null as 'NULL' csv header quote E'\b';
+\copy ratings from program 'gzip -dc title.ratings.tsv.gz' delimiter E'\t' null as 'NULL' csv header quote E'\b';
+\copy people(nconst, primaryname, birthyear, deathyear, primaryprofession, knownfortitles) from program 'gzip -dc name.basics.tsv.gz' delimiter E'\t' null as 'NULL' csv header quote E'\b';
+\copy principals from program 'gzip -dc title.principals.tsv.gz' delimiter E'\t' null as 'NULL' csv header quote E'\b';
+\copy crew from program 'gzip -dc title.crew.tsv.gz' delimiter E'\t' null as 'NULL' csv header quote E'\b';
